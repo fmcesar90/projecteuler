8cc5d9505c23e25951a46d42bdda243d13a8df4d
Frank Manuel <frankiwork90@gmail.com>
2022-10-11 07:04:57 +0100

    Even Fibonacci numbers

--- projecteuler/Program.cs
+++ projecteuler/Program.cs
@@ -1,9 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+//fn = fn-1 + fn-2
+//f3 = f2 + f1
+int num = 1;
 int sum = 0;
-for (int i = 1; i < 1000; i++)
+int result = 0;
+while (sum < 4000000)
 {
-    if (i % 3 == 0 || i % 5 == 0)
-        sum += i;
+    sum = Fibonacci(num);
+    if (sum % 2 == 0)
+        result += sum;
+    num++;
 }
-Console.WriteLine(sum);
+Console.WriteLine(result);
+int Fibonacci(int num)
+{
+    if (num <= 1)
+        return num;
+    return Fibonacci(num - 2) + Fibonacci(num - 1);
+}
